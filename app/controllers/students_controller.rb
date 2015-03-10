ea19dc56fea69b8788e6b45744a2bb26a46417fc
Jennifer Humphrey <jen.e.humphrey@gmail.com>
2015-03-10 16:50:59 -0600

    refactored method in model

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -2,8 +2,10 @@ class StudentsController < ApplicationController
 
   def index
     @random_groups = Student.random_group
-
-    
   end
 
+  def create
+    @random_groups = Student.random_group(params[:group][:size])
+    render :index
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   root 'students#index'
+  get '/', to: 'students#index'
+  post '/', to: 'students#create'
 end
