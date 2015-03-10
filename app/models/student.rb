f89ae87e9f14d570a634ab3863f95162ba8e015a
Jennifer Humphrey <jen.e.humphrey@gmail.com>
2015-03-10 17:30:52 -0600

    "bootstrap"

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,6 +2,6 @@ class Student < ActiveRecord::Base
 
   def self.random_group(n = 2)
     array = ["Alex E", "Steve", "Jennifer", "Dylan", "Jeffrey", "Charlie", "Ian", "Dan", "Ben", "Chloe", "Eric", "Pamela", "Annie", "Reece", "Kate", "Katie", "Amy", "Alex L", "Austin", "Chase", "Dillon", "Teddi", "Kyle", "Peter C", "Peter F", "Tristan", "Melani", "Dave"]
-    array.shuffle.each_slice(n.to_i).to_a
+    array.shuffle.each_slice(n.to_i)
   end
 end
