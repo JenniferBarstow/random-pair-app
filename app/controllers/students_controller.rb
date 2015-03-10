7ae03513aa75b2b08db0547e3901ea200ae8c3dc
Jennifer Humphrey <jen.e.humphrey@gmail.com>
2015-03-10 15:02:01 -0600

    added slice and shuffle method

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,7 +1,9 @@
 class StudentsController < ApplicationController
 
   def index
+    @random_groups = Student.random_group
 
+    
   end
 
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,11 @@
 class Student < ActiveRecord::Base
 
+
+  def self.random_group
+    array = ["Alex E", "Steve", "Jennifer", "Dylan", "Jeffrey", "Charlie", "Ian", "Dan", "Ben", "Chloe", "Eric", "Pamela", "Annie", "Reece", "Kate", "Katie", "Amy", "Alex L", "Austin", "Chase", "Dillon", "Teddi", "Kyle", "Peter C", "Peter F", "Tristan", "Melani"]
+    array.shuffle.each_slice(2).to_a
+
+  end
+
+
 end
