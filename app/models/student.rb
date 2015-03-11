314cfa9dc6be15f0859d86b50e48a4a32a287947
Alex Emrie <emrieaj@gmail.com>
2015-03-10 18:20:58 -0600

    Added join function on index page splitting students

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,7 +1,9 @@
 class Student < ActiveRecord::Base
 
+    # array = ["Alex E.", "Alex L.", "Amy B.", "Annie L.", "Austin A.", "Ben S.", "Charlie P.", "Chase T.", "Chloe B.", "Dan M.", "Dave A.", "Dillon G.", "Dylan H.", "Eric B.", "Ian B.", "Jeffrey M.", "Jennifer H.", "Kate K.V.", "Katie P.", "Kyle D.", "Melani D.", "Pamela A.", "Peter C.", "Peter F.", "Reece V.C.", "Steve H.", "Teddi M.", "Tristan R." ]
+
   def self.random_group(n = 2)
-    array = ["Alex E", "Steve", "Jennifer", "Dylan", "Jeffrey", "Charlie", "Ian", "Dan", "Ben", "Chloe", "Eric", "Pamela", "Annie", "Reece", "Kate", "Katie", "Amy", "Alex L", "Austin", "Chase", "Dillon", "Teddi", "Kyle", "Peter C", "Peter F", "Tristan", "Melani", "Dave"]
+  array = ["Alex E", "Steve", "Jennifer", "Dylan", "Jeffrey", "Charlie", "Ian", "Dan", "Ben", "Chloe", "Eric", "Pamela", "Annie", "Reece", "Kate", "Katie", "Amy", "Alex L", "Austin", "Chase", "Dillon", "Teddi", "Kyle", "Peter C", "Peter F", "Tristan", "Melani", "Dave"]
     array.shuffle.each_slice(n.to_i)
   end
 end
