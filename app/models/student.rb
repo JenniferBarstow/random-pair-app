5a05b9965c052ee9232797fc49c0851cce0f5d08
Jennifer Humphrey <jen.e.humphrey@gmail.com>
2015-03-10 18:57:19 -0600

    added students last names

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,7 +1,10 @@
 class Student < ActiveRecord::Base
 
   def self.random_group(n = 2)
-    array = ["Alex E", "Steve", "Jennifer", "Dylan", "Jeffrey", "Charlie", "Ian", "Dan", "Ben", "Chloe", "Eric", "Pamela", "Annie", "Reece", "Kate", "Katie", "Amy", "Alex L", "Austin", "Chase", "Dillon", "Teddi", "Kyle", "Peter C", "Peter F", "Tristan", "Melani", "Dave"]
+    array = ["Alex Emrie", "Alexandra Laudon", "Amy Bertken", "Annie Lydens", "Austin Adams", "Ben Steinbook", "Charles Painter", "Chase Terzian", "Chloe Bradley", "Dan Morrisett",
+             "David Abramowitz", "Dillon Good", "Dylan Ham", "Eric Bartsch", "Ian Brennan", "Jeffrey Mince", "Jennifer Humphrey", "Kate Killian Vovk",
+             "Katherine Pekarek", "Kyle DeFauw", "Melani Dizon", "Pamela Adams", "Peter Cardi", "Peter Furey", "Reece Van Cleve", "Steve Hirschhorn",
+             "Teddi Maul", "Tristan Rubadeau"]
     array.shuffle.each_slice(n.to_i)
   end
 end
